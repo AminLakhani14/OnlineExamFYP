9362a223bfafbbdc8a50e7058a4a9603cf271108
Amin <aminlakhani254@gmail.com>
2023-04-05 23:02:21 +0500

    evaluation is done of QA and MCQs

--- src/app/views/AttemptExam/AttemptMCQs/AttemptMCQ.jsx
+++ src/app/views/AttemptExam/AttemptMCQs/AttemptMCQ.jsx
@@ -16,92 +16,144 @@ import {
   TextareaAutosize,
   TextField,
   Typography,
-} from "@mui/material";
-import SaveIcon from '@mui/icons-material/Save';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { Modal } from "antd";
-import HeadBreadCrumb from "app/components/BreadCrumb/HeadBreadCrumb";
-import { useState } from "react";
-import axios from "axios";
-import { useEffect } from "react";
+} from '@mui/material'
+import SaveIcon from '@mui/icons-material/Save'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
+import { Modal } from 'antd'
+import HeadBreadCrumb from 'app/components/BreadCrumb/HeadBreadCrumb'
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import DialogBox from 'app/components/DialogBox/DialogBox'
+import { useNavigate } from 'react-router-dom'
 
 const AttemptMCQ = () => {
   const [tableData, setTableData] = useState([])
   const [currentQuestion, setcurrentQuestion] = useState({})
-  const [ScoreModal, setScoreModal] = useState(false);
+  const [ScoreModal, setScoreModal] = useState(false)
+  const [selectedOption, setSelectedOption] = useState('')
+  const [openDialogbox, setOpenDialogBox] = useState(false)
+  const [Obtainedmarks, setObtainedmarks] = useState(0)
+  const navigate=useNavigate()
 
-  const showScoreModal = () => {
-    setScoreModal(true);
-  };
-  
-  
   const handleOk = () => {
-    setScoreModal(false);
-  };
-  
+    setScoreModal(false)
+    localStorage.setItem('MCQsMarks',Obtainedmarks)
+    navigate("/")
+  }
+
   const handleCancel = () => {
-    setScoreModal(false);
-  };
- 
+    setScoreModal(false)
+  }
+
   const getpost = () => {
-    debugger;
-  
+    debugger
+
     axios
       .get(`https://localhost:7040/api/MCQs/Get-MCQs`)
       .then((res) => {
-        debugger;
+        debugger
         if (res.status === 200) {
-          if(res.data.length){
-            setTableData(res.data);
+          if (res.data.length) {
+            setTableData(res.data)
             setcurrentQuestion(res.data[0])
           }
-          console.log("hello world", res.data);
+          console.log('hello world', res.data)
         } else {
-          console.log("something went wrong");
+          console.log('something went wrong')
         }
       })
       .catch((err) => {
-        debugger;
-        console.error(err);
-      });
-  };
-  
+        debugger
+        console.error(err)
+      })
+  }
+
   useEffect(() => {
-    getpost();
+    getpost()
   }, [])
-  
+
   console.log(tableData)
 
-  const  clickNextQuestion=(e)=>{
-    if(currentQuestion?.id===tableData.slice(-1)[0]?.id ){
-      showScoreModal();
-      // handle submit
-    }else{
-      // handle next question
-      let array=[...tableData]
-      let obj=array.find(x=>x.id===currentQuestion.id+1)
-      debugger
-      setcurrentQuestion(obj)
-      
+  const handleRadio = (e) => {
+    let { value } = e.target
+    setSelectedOption(value)
+  }
+
+  const clickNextQuestion = (e) => {
+    if (!selectedOption) {
+      setOpenDialogBox(true)
+    } else {
+      handleSaveResult()
     }
   }
+
+  const handleSaveResult = () => {
+    debugger
+    let array = [...tableData]
+    let selectedQuestion = array.find((x) => x.id === currentQuestion.id)
+    if (selectedOption === selectedQuestion.correctAnswer) {
+      setObtainedmarks((prev) => prev + +selectedQuestion.marks)
+    } else {
+      setObtainedmarks((prev) => prev + 0)
+    }
+    if (currentQuestion?.id !== array.slice(-1)[0]?.id) {
+      setSelectedOption('')
+      let index = array.findIndex((x) => x.id === currentQuestion.id)
+      let obj1 = array[index + 1]
+      setcurrentQuestion(obj1)
+    } else {
+      setScoreModal(true)
+    }
+  }
+
+  const closeDialogBox = () => {
+    setOpenDialogBox(false)
+  }
+
   return (
     <>
-        <Modal title="Result" open={ScoreModal} okText="Ok" onCancel={handleCancel}
-         onOk={handleOk}>
-    <Box>
-          <Typography id="keep-mounted-modal-title" variant="h6" component="h2" className="mt-3 mb-4">
-            Your Marks is 10/10
-          </Typography>
-          <div className="mb-4">
-        </div>
-        </Box>
-    </Modal>
-      <HeadBreadCrumb text1={"Dashboard"} text2={"MCQs"} url={"/"} />
-     <div className="row m-0">
+      {openDialogbox && (
+        <DialogBox
+          openDialog={openDialogbox}
+          title="Please fill the answer first"
+          handleOk={closeDialogBox}
+          Okaybtn={true}
+        />
+      )}
+      {ScoreModal && (
+        <Modal
+          title="Result"
+          open={ScoreModal}
+          okText="Ok"
+          closable={false}
+          footer={[
+            <Button key="ok" type="secondary" onClick={handleOk}>
+              OK
+            </Button>,
+          ]}
+        >
+          <Box>
+            <Typography
+              id="keep-mounted-modal-title"
+              variant="h6"
+              component="h2"
+              className="mt-3 mb-4"
+            >
+              Your Marks is {Obtainedmarks}/{' '}
+              {tableData.reduce(function (acc, obj) {
+                return acc + +obj.marks
+              }, 0)}
+            </Typography>
+            <div className="mb-4"></div>
+          </Box>
+        </Modal>
+      )}
+      <HeadBreadCrumb text1={'Dashboard'} text2={'MCQs'} url={'/'} />
+      <div className="row m-0">
         <div className="d-flex justify-content-between mt-4">
           <h3 id="QuestionText" className="m-0 mt-2">
-          Question {currentQuestion?.id ?? ''}:
+            Question {currentQuestion?.id ?? ''}:
           </h3>
           <div className="align-items-center d-flex justify-content-end w-auto">
             <label className="align-items-center mt-2 d-flex px-2">
@@ -111,11 +163,11 @@ const AttemptMCQ = () => {
               className="mt-2 w-50"
               fullWidth
               disabled
-              value={currentQuestion?.marks ?? ""}
+              value={currentQuestion?.marks ?? ''}
               size="small"
               sx={{
-                "& legend": { display: "none" },
-                "& fieldset": { top: 0 },
+                '& legend': { display: 'none' },
+                '& fieldset': { top: 0 },
               }}
             />
           </div>
@@ -123,7 +175,7 @@ const AttemptMCQ = () => {
       </div>
       <div className="row m-0">
         <div className="col-12">
-            <h6>{currentQuestion?.question ?? ""}</h6>
+          <h6>{currentQuestion?.question ?? ''}</h6>
         </div>
       </div>
 
@@ -140,28 +192,29 @@ const AttemptMCQ = () => {
           <FormControl>
             <RadioGroup
               aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="female"
               name="radio-buttons-group"
+              onChange={handleRadio}
             >
               <FormControlLabel
-               label={currentQuestion?.optionA ?? ""}
+                label={currentQuestion?.optionA ?? ''}
                 control={<Radio />}
-                value="1"
+                value={currentQuestion?.optionA ?? ''}
               />
-              <FormControlLabel 
-              label={currentQuestion?.optionB ?? ""}
-               control={<Radio />} 
-               value="2" />
+              <FormControlLabel
+                label={currentQuestion?.optionB ?? ''}
+                control={<Radio />}
+                value={currentQuestion?.optionB ?? ''}
+              />
 
               <FormControlLabel
-              label={currentQuestion?.optionC ?? ""}
+                label={currentQuestion?.optionC ?? ''}
                 control={<Radio />}
-                value="3"
+                value={currentQuestion?.optionC ?? ''}
               />
               <FormControlLabel
-                label={currentQuestion?.optionD ?? ""}
+                label={currentQuestion?.optionD ?? ''}
                 control={<Radio />}
-                value="4"
+                value={currentQuestion?.optionD ?? ''}
               />
             </RadioGroup>
           </FormControl>
@@ -172,20 +225,30 @@ const AttemptMCQ = () => {
       <br />
       <br />
       <div className="d-flex justify-content-end footerBtn">
-          <Button
-            variant="outlined"
-            sx={{
-              width: 200,
-              color: currentQuestion?.id===tableData.slice(-1)[0]?.id ? "red" :"blue",
-            }}
-            endIcon={currentQuestion?.id===tableData.slice(-1)[0]?.id ?  <SaveIcon /> :  <NavigateNextIcon />}
-            onClick={clickNextQuestion}
-          >
-          {currentQuestion?.id===tableData.slice(-1)[0]?.id ? "Submit" : "Next Question" }
-          </Button>
+        <Button
+          variant="outlined"
+          sx={{
+            width: 200,
+            color:
+              currentQuestion?.id === tableData.slice(-1)[0]?.id
+                ? 'red'
+                : 'blue',
+          }}
+          endIcon={
+            currentQuestion?.id === tableData.slice(-1)[0]?.id ? (
+              <SaveIcon />
+            ) : (
+              <NavigateNextIcon />
+            )
+          }
+          onClick={clickNextQuestion}
+        >
+          {currentQuestion?.id === tableData.slice(-1)[0]?.id
+            ? 'Submit'
+            : 'Next Question'}
+        </Button>
       </div>
-      
     </>
-  );
-};
-export default AttemptMCQ;
+  )
+}
+export default AttemptMCQ
--- src/app/views/AttemptExam/AttemptQA/AttemptQA.jsx
+++ src/app/views/AttemptExam/AttemptQA/AttemptQA.jsx
@@ -23,6 +23,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import DialogBox from "app/components/DialogBox/DialogBox";
+import { useNavigate } from "react-router-dom";
 
 const AttemptQA = () => {
 
@@ -30,20 +31,34 @@ const AttemptQA = () => {
   const [openDialogbox, setOpenDialogBox] = useState(false);
   const [answerData, setanswerData] = useState("");
   const [Obtainedmarks,setObtainedmarks]=useState(0)
-  const showScoreModal = () => {
-    setScoreModal(true);
-  };
-  
-  
+  const navigate =useNavigate()
   const handleOk = () => {
     setScoreModal(false);
+    localStorage.setItem('QAMarks',Obtainedmarks)
+    navigate('/')
   };
   
   const handleCancel = () => {
     setScoreModal(false);
+    localStorage.setItem('QAMarks',Obtainedmarks)
+    navigate('/')
   };
   const [tableData, setTableData] = useState([])
   const [currentQuestion, setcurrentQuestion] = useState({})
+
+  const [time, setTime] = useState(300);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTime((time) => {
+        if (time === 0) {
+          clearInterval(timer);
+          setScoreModal(true);
+          return 0;
+        } else return time - 1;
+      });
+    }, 1000);
+  }, []);
  
   const closeDialogBox=()=>{
     setOpenDialogBox(false)
@@ -81,13 +96,7 @@ const AttemptQA = () => {
     if(!answerData){
       setOpenDialogBox(true)
     }else{
-      if(currentQuestion?.id===tableData.slice(-1)[0]?.id ){
-        showScoreModal();
-        // handle submit
-      }else{
-        // handle next question
         handleSaveResult()
-      }
     }
 
   }
@@ -95,21 +104,25 @@ const AttemptQA = () => {
     let array=[...tableData]
     let obj=array.find(x=>x.id===currentQuestion.id)
     let answerStr=answerData.split(" ")
-    let result=answerStr.filter(x=>x.toLowerCase() ===obj.keyword1.toLowerCase() || x===obj.keyword2.toLowerCase() || x===obj.keyword3.toLowerCase() || x===obj.keyword4.toLowerCase()|| x===obj.keyword5.toLowerCase())
-    console.log(result,"result")
+    let result=answerStr.filter(x=>x.toLowerCase() ===obj.keyword1.trim().toLowerCase() || x===obj.keyword2.trim().toLowerCase() || x===obj.keyword3.trim().toLowerCase() || x===obj.keyword4.trim().toLowerCase()|| x===obj.keyword5.trim().toLowerCase())
     if(result.length===5 || answerData===obj.answer){
       setObtainedmarks(prev=> prev + obj.marks)
-      console.log(Obtainedmarks + +obj.marks,"result")
     }else{
       let calculateMarks=obj.marks/5
       let getMarks= result.length===1 ? calculateMarks : calculateMarks * result.length
-      console.log(getMarks,"getMarks")
       setObtainedmarks(prev=> prev + getMarks)
     }
-    // let obj1=array.find(x=>x.id===currentQuestion.id+1)
-    // setcurrentQuestion(obj1)
-  }
+    if(currentQuestion?.id!==tableData.slice(-1)[0]?.id ){
+      setanswerData("")
+      let index=array.findIndex(x=>x.id===currentQuestion.id)
+      let obj1=array[index+1]
 
+      setcurrentQuestion(obj1)
+    }else{
+       setScoreModal(true);
+    }
+  }
+  
   return (
     <>
    {openDialogbox &&
@@ -119,22 +132,40 @@ const AttemptQA = () => {
     handleOk={closeDialogBox}
     Okaybtn={true}
     />}
-    <Modal title="Result" open={ScoreModal} okText="Ok" onCancel={handleCancel}
-         onOk={handleOk}>
-    <Box>
+   {ScoreModal &&
+     <Modal title="Result"
+      open={ScoreModal} 
+      okText="Ok" 
+      closable={false}
+      footer={[
+        <Button key="ok" type="secondary" onClick={handleOk}>
+          OK
+        </Button>,
+      ]}
+      >
+        <Box>
           <Typography id="keep-mounted-modal-title" variant="h6" component="h2" className="mt-3 mb-4">
-            Your Marks is 10/10
+            Your Marks is {Obtainedmarks.toFixed(2)}/ {tableData.reduce(function (acc, obj) { return acc + +obj.marks; }, 0)}
           </Typography>
           <div className="mb-4">
         </div>
         </Box>
-    </Modal>
+    </Modal>}
       <HeadBreadCrumb text1={"Dashboard"} text2={"Question/Answer"} url={"/"} />
+      <div className="row m-2">
+      <div className="col-12">
+      <p className="text-center text-bg-danger">
+      Time left: {`${Math.floor(time / 60)}`.padStart(2, 0)}:
+      {`${time % 60}`.padStart(2, 0)}
+      </p>
+      </div>
+      </div>
+
       <div className="row m-2">
         <div className="col-12">
           <div className="d-flex justify-content-between">
             <h3 id="QuestionText" className="mt-2">
-              Question {currentQuestion?.id ?? ''}:
+              Question 
             </h3>
             <div className="align-items-center d-flex justify-content-end w-auto">
               <label className="align-items-center mt-2 d-flex px-2">
