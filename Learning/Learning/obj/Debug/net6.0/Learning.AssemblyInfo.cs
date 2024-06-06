45a9d100e3680063aeaf9c7946bf56c0337a1568
Amin Lakhani <aminlakhani254@gmail.com>
2024-06-06 12:11:25 +0500

    Amin

--- Learning/Learning/obj/Debug/net6.0/Learning.AssemblyInfo.cs
+++ Learning/Learning/obj/Debug/net6.0/Learning.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9525e5ecebd424fce6a64c93f1740a311ac12c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
